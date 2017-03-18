63e292674a84a4bd2c0464624623755240d9b7f6
Peter Young <young40@gmail.com>
2017-03-19 00:44:52 +0800

    配置lua的补全功能

--- private/lua-config/packages.el
+++ private/lua-config/packages.el
@@ -1,5 +1,7 @@
 (setq lua-config-packages
-      '(lua-mode))
+      '(
+        lua-mode
+        company))
 
 (defun lua-config/post-init-lua-mode()
   (use-package lua-mode
@@ -8,5 +10,28 @@
     (progn
       (setq lua-indent-level 2)
       (define-key lua-mode-map (kbd "s-r") 'lua-send-buffer)
+
+      (push '(lua-mode "getmetatable" "ipairs" "module" "pairs" "print"
+                       "rawequal" "rawget" "rawlen" "rawset" "require"
+                       "setmetatable" "tonumber" "tostring" "type" "unpack" "self"
+                       "coroutine" "create" "isyieldable" "resume" "running" "status"
+                       "wrap" "yield"
+                       "math" "abs" "acos" "asin" "atan" "atan2" "ceil" "cos" "cosh"
+                       "deg" "exp" "floor" "fmod" "frexp" "huge" "ldexp" "log"
+                       "log10" "max" "maxinteger" "min" "mininteger" "modf" "pi"
+                       "pow" "rad" "random" "randomseed" "sin" "sinh" "sqrt"
+                       "tan" "tanh" "tointeger" "type" "ult"
+                       "package" "config" "cpath" "loaded" "loaders" "loadlib" "path"
+                       "preload" "searchers" "searchpath" "seeall"
+                       "string" "byte" "char" "dump" "find" "format" "gmatch" "gsub"
+                       "len" "lower" "match" "pack" "packsize" "rep" "reverse"
+                       "sub" "unpack" "upper"
+                       "table" "concat" "insert" "maxn" "move" "pack" "remove" "sort"
+                       "unpack"
+                       ) company-keywords-alist)
       )
     ))
+
+(defun lua-config/post-init-company()
+  (spacemacs|add-company-backends :backends company-capf
+                                  :modes lua-mode))
