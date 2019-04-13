b230a474ae44d0ef73e1d481c80e88c5c00f1632
Peter Young <young40@gmail.com>
2019-04-13 15:21:48 +0800

    lua 换用 language server protocol

--- private/lua-config/packages.el
+++ private/lua-config/packages.el
@@ -16,6 +16,7 @@
 (defun lua-config/init-lua-mode ()
   (use-package lua-mode
     :defer t
+    :after lsp-mode
     :mode ("\\.lua\\'" . lua-mode)
     :interpreter ("lua" . lua-mode)
     :hook (lua-mode . company-lua-mode-setup)
@@ -73,28 +74,9 @@
        '(tab-width 4))
 
       (define-key lua-mode-map (kbd "s-r") 'lua-send-buffer)
-
-      (push '(lua-mode "getmetatable" "ipairs" "module" "pairs" "print"
-                       "rawequal" "rawget" "rawlen" "rawset" "require"
-                       "setmetatable" "tonumber" "tostring" "type" "unpack" "self"
-                       "coroutine" "create" "isyieldable" "resume" "running" "status"
-                       "wrap" "yield"
-                       "math" "abs" "acos" "asin" "atan" "atan2" "ceil" "cos" "cosh"
-                       "deg" "exp" "floor" "fmod" "frexp" "huge" "ldexp" "log"
-                       "log10" "max" "maxinteger" "min" "mininteger" "modf" "pi"
-                       "pow" "rad" "random" "randomseed" "sin" "sinh" "sqrt"
-                       "tan" "tanh" "tointeger" "type" "ult"
-                       "package" "config" "cpath" "loaded" "loaders" "loadlib" "path"
-                       "preload" "searchers" "searchpath" "seeall"
-                       "string" "byte" "char" "dump" "find" "format" "gmatch" "gsub"
-                       "len" "lower" "match" "pack" "packsize" "rep" "reverse"
-                       "sub" "unpack" "upper"
-                       "table" "concat" "insert" "maxn" "move" "pack" "remove" "sort"
-                       "unpack"
-                       ) company-keywords-alist)
       )
     ))
 
-(defun lua-config/post-init-company()
-  (spacemacs|add-company-backends :backends company-capf
-                                  :modes lua-mode))
+;; (defun lua-config/post-init-company()
+  ;; (spacemacs|add-company-backends :backends company-capf
+                                  ;; :modes lua-mode))
