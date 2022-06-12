211e95ac366e737a39d5585579b824e280446cc5
Bourne Young <young40@gmail.com>
2022-06-12 18:25:55 +0800

    reinit .emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,8 +21,6 @@
 (package-install 'consult)
 (global-set-key (kbd "C-s") 'consult-line)
 
-;;(package-install 'company)
-
 (setq make-backup-files nil)
 
 (delete-selection-mode 1)
@@ -55,12 +53,8 @@
   (find-file "/Users/young40/dot/Spacemacs/.emacs.d/init.el"))
 (global-set-key (kbd "s-e") 'open-init-file)
 
-;;(global-company-mode 1)
 
 (fset 'gnus-yes-or-no-p 'y-or-n-p)
-
-;;(define-key company-active-map (kbd "C-n") 'company-select-next)
-;;(define-key company-active-map (kbd "C-p") 'company-select-previous)
 
 (set-face-attribute 'default nil :height 160)
 ;;让鼠标滚动更好用
