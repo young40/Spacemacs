7622507755766a71310f3161af92e13a5c08d0d5
Bourne Young <young40@gmail.com>
2022-06-12 17:23:35 +0800

    reinit .emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,7 @@
 (package-install 'consult)
 (global-set-key (kbd "C-s") 'consult-line)
 
-(package-install 'company)
+;;(package-install 'company)
 
 (setq make-backup-files nil)
 
@@ -55,12 +55,12 @@
   (find-file "/Users/young40/dot/Spacemacs/.emacs.d/init.el"))
 (global-set-key (kbd "s-e") 'open-init-file)
 
-(global-company-mode 1)
+;;(global-company-mode 1)
 
 (fset 'gnus-yes-or-no-p 'y-or-n-p)
 
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
+;;(define-key company-active-map (kbd "C-n") 'company-select-next)
+;;(define-key company-active-map (kbd "C-p") 'company-select-previous)
 
 (set-face-attribute 'default nil :height 160)
 ;;让鼠标滚动更好用
--- .emacs.d/lisp/init-packages.el
+++ .emacs.d/lisp/init-packages.el
@@ -5,8 +5,23 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(package-install 'use-package)
+(add-to-list 'load-path "/Users/young40/dot/Spacemacs/.emacs.d/use-package")
+(add-to-list 'load-path "/Users/young40/dot/Spacemacs/.emacs.d/restart-emacs")
+(require 'use-package)
+;;(package-install 'use-package)
 
 (message "ok")
+
+(use-package company
+  :bind (:map company-active-map
+	      ("C-n" . 'company-select-next)
+	      ("C-p" . 'company-select-previous))
+  :init
+  (global-company-mode t)
+  :config
+  (setq company-minimum-prefix-length 1)
+  (setq company-idle-delay 0))
+
+(require 'restart-emacs)
 
 (provide 'init-packages)
