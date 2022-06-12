5f4e9440ff0786e6613c432d7f1827cde08ae581
Bourne Young <young40@gmail.com>
2022-06-13 01:03:18 +0800

    reinit .emacs.d

--- .emacs.d/lisp/init-ui.el
+++ .emacs.d/lisp/init-ui.el
@@ -13,14 +13,27 @@
   (progn (add-hook 'prog-mode-hook 'rainbow-identifiers-mode))
   )
 
-(use-package powerline-evil
-  :ensure t
-  :config
-  (powerline-default-theme)
+;; (use-package powerline-evil
+;;   :ensure t
+;;   :config
+;;   (powerline-default-theme)
   ;; (powerline-evil-center-color-theme)
   ;; (powerline-evil-vim-color-theme)
   ;; (powerline-evil-vim-theme)
-  )
+  ;; )
+
+;;modeline上显示我的所有的按键和执行的命令
+(package-install 'keycast)
+(add-to-list 'global-mode-string '("" keycast-mode-line))
+(keycast-mode t)
+
+(use-package doom-modeline
+  :ensure t
+  :custom-face
+(mode-line ((t (:height 0.9))))
+(mode-line-inactive ((t (:height 0.9))))
+  :init
+  (doom-modeline-mode t))
 
 (add-hook 'prog-mode-hook 'linum-mode)
 
