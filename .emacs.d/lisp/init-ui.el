0a101ee2ecb7d2059cad699199a00796fadb9bb4
Bourne Young <young40@gmail.com>
2022-06-13 00:06:43 +0800

    reinit .emacs.d

--- .emacs.d/lisp/init-ui.el
+++ .emacs.d/lisp/init-ui.el
@@ -1,4 +1,21 @@
 (use-package monokai-pro-theme :ensure t :config (load-theme 'monokai-pro t))
 ;; (use-package monokai-theme :ensure t :config (load-theme 'monokai t))
 
+(use-package rainbow-identifiers
+  :ensure t
+  :init
+  (progn (add-hook 'prog-mode-hook 'rainbow-identifiers-mode))
+  )
+
+(use-package powerline-evil
+  :ensure t
+  :config
+  (powerline-default-theme)
+  ;; (powerline-evil-center-color-theme)
+  ;; (powerline-evil-vim-color-theme)
+  ;; (powerline-evil-vim-theme)
+  )
+
+(add-hook 'prog-mode-hook 'linum-mode)
+
 (provide 'init-ui)
