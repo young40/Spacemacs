65984039fb212819ef35c0e4ddbebf863cdf1499
Bourne Young <young40@gmail.com>
2022-06-18 16:43:13 +0800

    reinit .emacs.d

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,4 +1,9 @@
 ;; 会在 init.el 前加载
 
+(setq gc-cons-percentage 1000000000)
+
 (setq package-enable-at-startup nil)
+
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
 ;; (message "early init.")
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -57,7 +57,6 @@
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
 (setq mouse-wheel-progressive-speed nil)
 
-(add-hook 'window-setup-hook 'toggle-frame-maximized t)
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 
 (setq custom-file (expand-file-name (concat *ewc* "custom.el")))
